6af6112e3202314ec7544b922eb97d079c3b20c9
Kevin Fallas <Kevin.fallas.b@gmail.com>
2022-12-07 17:11:03 -0600

    Change ec2 props

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,11 +1,10 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "ec2-pagina-web" {
   # Block body
   ami           = var.ami-id
   instance_type = var.instance-type
   key_name      = var.key_pair_name
-  #user_data     = file("../scripts/initial-setup.sh")
-
+  
   tags = {
-    "Name" = "EC2 CI/CD"
+    "Name" = "ec2-pagina-web"
   }
 }
