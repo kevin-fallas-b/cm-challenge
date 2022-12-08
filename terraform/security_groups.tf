2a7ad7d3a46383d69fce2bfc04003ea3ee2ced56
Kevin Fallas <Kevin.fallas.b@gmail.com>
2022-12-08 16:23:10 -0600

    Fix bug in security group tf file

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -2,21 +2,21 @@ resource "aws_security_group" "nginx" {
     name = var.sg_name
     vpc_id = local.vpc_id
 
-    ingress = {
+    ingress  {
         from_port = 22
         to_port = 22
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
     
-    ingress = {
+    ingress  {
         from_port = 80
         to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    egress = {
+    egress  {
       from_port = 0
       to_port = 0
       protocol = "-1"
